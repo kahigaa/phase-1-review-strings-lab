9eb99204a8a05202124ca44bdb1ee009d1addf9c
Adrian Kahiga <kahigaadrian7@gmail.com>
2025-04-01 10:33:00 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "JohnDoe";
+const welcomeMessage = `Welcome to FlatBook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
